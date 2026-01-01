0042bf3946e22de9473773cfedeb4a547cee8886
saikumar pillalamarri <pillalamarrisaikumar8@gmail.com>
2026-01-01 16:09:01 +0530

    X-Calculator

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -82,24 +82,7 @@ const handleEqual = () => {
                 ))}
             </div>
            ))}
-            {/* <div className={"row2"} style={{marginTop:"10px"}}>
-                <button style={{margin:10, padding:32,borderRadius:"8px",border:"2px solid green",cursor:"pointer"}} onClick={()=>handleClick("4")}>4</button>
-                 <button style={{margin:10, padding:32,borderRadius:"8px",border:"2px solid green",cursor:"pointer"}} onClick={()=> handleClick("5")}>5</button>
-                  <button style={{margin:10, padding:32,borderRadius:"8px",border:"2px solid green",cursor:"pointer"}} onClick={()=> handleClick("6")}>6</button>
-                   <button style={{margin:10, padding:32, borderRadius:"8px", border:"2px solid green",cursor:"pointer"}} onClick={()=>handleClick("-")}>-</button>
-            </div>
-            <div className={"row3"} style={{marginTop:"10px"}}>
-                <button style={{margin:10, padding:32,borderRadius:"8px",border:"2px solid green",cursor:"pointer"}} onClick={()=> handleClick("1")}>1</button>
-                 <button style={{margin:10, padding:32,borderRadius:"8px",border:"2px solid green",cursor:"pointer"}} onClick={()=> handleClick("2")}>2</button>
-                  <button style={{margin:10, padding:32,borderRadius:"8px",border:"2px solid green",cursor:"pointer"}} onClick={()=> handleClick("3")}>3</button>
-                   <button style={{margin:10, padding:32, borderRadius:"8px", border:"2px solid green",cursor:"pointer"}} onClick={()=> handleClick("*")}>*</button>
-            </div>
-            <div className={"row4"} style={{marginTop:"10px"}}>
-                <button style={{margin:10, padding:32,borderRadius:"8px",border:"2px solid green",cursor:"pointer"}} onClick={handleClear}>C</button>
-                 <button style={{margin:10, padding:32,borderRadius:"8px",border:"2px solid green",cursor:"pointer"}} onClick={()=> handleClick("0")}>0</button>
-                  <button style={{margin:10, padding:32,borderRadius:"8px",border:"2px solid green",cursor:"pointer"}} onClick={handleEqual}>=</button>
-                   <button style={{margin:10, padding:32, borderRadius:"8px", border:"2px solid green",cursor:"pointer"}} onClick={()=> handleClick("/")}>/</button>
-            </div> */}
+            
         </div>
     )
 }
